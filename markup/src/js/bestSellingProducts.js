44073e96a5b194583c88972e4f4456306d25bfac
Nikita Maslov <maslov.n.e@gmail.com>
2020-10-08 17:23:09 +0300

    add updates

---
+++ markup/src/js/bestSellingProducts.js
@@ -0,0 +1,40 @@
+module.exports = () => {
+    const hearts = document.querySelectorAll(".products-small-cards__item-wishlist-btn");
+    const filledHearts = document.querySelectorAll(".products-small-cards__item-wishlist-btn-filled");
+    const texts = document.querySelectorAll(".products-small-cards__item-title");
+    
+
+      for(let i = 0; i < hearts.length; i++) {
+        hearts[i].addEventListener("click", function() {
+        if(changeFlag[i]) {
+            fill(i);
+        } else {
+            unfill(i);
+        }
+    });
+        filledHearts[i].addEventListener("click", function() {
+        if(changeFlag[i]) {
+            fill(i);
+        } else {
+            unfill(i);
+        }
+    });
+      }
+    
+    let changeFlag = [16];
+    for(let i = 0; i < hearts.length; i++) {
+      changeFlag[i] = true;
+    }
+    function fill(i) {
+      filledHearts[i].classList.add('products-small-cards__item-wishlist-btn--active');
+      hearts[i].classList.remove('products-small-cards__item-wishlist-btn--active');
+      texts[i].classList.add("products-small-cards__item-title--active");
+      changeFlag[i] = false;
+    }
+    function unfill(i) {
+      hearts[i].classList.add('products-small-cards__item-wishlist-btn--active');
+      filledHearts[i].classList.remove('products-small-cards__item-wishlist-btn--active');
+      texts[i].classList.remove('products-small-cards__item-title--active');
+      changeFlag[i] = true;
+    }
+}
---
+++ markup/src/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+const bestSellingProducts = require(`./bestSellingProducts`);
+
+bestSellingProducts();
